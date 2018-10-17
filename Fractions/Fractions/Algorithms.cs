35af5afe299c5dd4cc8ecb6082827bb60d31e066
Andrei Muntean <andreimuntean909@gmail.com>
2018-10-17 17:25:42 +0300

    done

---
+++ Fractions/Fractions/Algorithms.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractions
+{
+    class Algorithms
+    {
+        public static int Gcd (int a, int b)
+        {
+            int r;
+            a = Math.Abs(a);
+            b = Math.Abs(b);
+            do
+            {
+                r = a % b;
+                a = b;
+                b = r;
+            } while (r != 0);
+
+            return a;
+        }
+
+        public static int Lcm (int a, int b)
+        {
+            return (a * b) / Gcd(a, b);
+        }
+    }
+}
---
+++ Fractions/Fractions/Fraction.cs
@@ -0,0 +1,74 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Fractions
+{
+    class Fraction
+    {
+        private int _numerator;
+        private int _denominator;
+
+        public int Numerator
+        {
+            get
+            {
+                return _numerator;
+            }
+            set
+            {
+                _numerator = value;
+            }
+        }
+        public int Denominator
+        {
+            get
+            {
+                return _denominator;
+            }
+            set
+            {
+                if(value != 0)
+                    _denominator = value;
+            }
+        }
+
+        public Fraction()
+        {
+            Numerator = 1;
+            Denominator = 1;
+        }
+
+        public Fraction(int a, int b)
+        {
+            Numerator = a;
+            Denominator = b;
+        }
+
+        public void Reduction()
+        {
+            int gcd = Algorithms.Gcd(Numerator, Denominator);
+            Numerator /= gcd;
+            Denominator /= gcd;
+        }
+
+        public static Fraction operator+ (Fraction a, Fraction b)
+        {
+            a.Reduction(); b.Reduction();
+            var c = new Fraction();
+
+            int lcm = Algorithms.Lcm(a.Denominator, b.Denominator);
+            c.Denominator = lcm;
+            c.Numerator = (lcm / a.Denominator) * a.Numerator + (lcm / b.Denominator) * b.Numerator;
+            c.Reduction();
+            return c;
+        }
+
+        public override string ToString()
+        {
+            return "(" + this.Numerator.ToString() + ")/(" + this.Denominator.ToString() + ")";
+        }
+    }
+}
